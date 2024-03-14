878e391cd4a54e6cfc00c79b8b3f224722d55054
nero-bc <126649764+nero-bc@users.noreply.github.com>
2024-03-14 07:48:05 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-API_ID = 22741640  # api id
-API_HASH = "c435fe7ad0a6398ccd9337839a3666e3"  # api hash
+API_ID = 25695562  # api id
+API_HASH = "0b691c3e86603a7e34aae0b5927d725a"  # api hash
 
-BOT_TOKEN = "6979408047:AAEcf8A2ocQ5oH0FMnO5L6KBeXmYCZSksC4"  # bot token
+BOT_TOKEN = "6993336563:AAG0zsh0LrDWLxBtDfcB4bdyTyhdxvIr-Qo"  # bot token
 
 
 ## REDIS
@@ -9,6 +9,6 @@ HOST = "redis-15392.c304.europe-west1-2.gce.cloud.redislabs.com"  # redis host u
 PORT = "15392"  # redis port
 PASSWORD = "cbw7xHuW3uJmddWJRRqxiSWmRvKi81GM"  # redis password
 
-PRIVATE_CHAT_ID = -1002095112878  # CHAT WHERE YOU WANT TO STORE VIDEOS
+PRIVATE_CHAT_ID = -1002059279603  # CHAT WHERE YOU WANT TO STORE VIDEOS
 COOKIE = "csrfToken=BnQx-ffuJKamkWLrsx3Ksds4; browserid=qEGrcJcpudjTVSp34yywiOK8KGczvjGYTPI6jJMydd7dpoQVUXCzDlp7JFM=; lang=en; __bid_n=18db31bdad1e0ee8094207; __stripe_mid=a300821a-4b38-4e0a-a227-5c27f1b7c54789f06e; __stripe_sid=f7b244b5-18a8-4540-bfa0-222b4acc0bf5eb56ba; ndus=Yb47bKTteHuiwDuAJYscQHtlthgbg2-2R4yRRWFe; ndut_fmt=17DAF2FD0DC6DF723D10EB645D49FD8581EFDCCB4A2823D11359EA0CCE270FBE"  # COOKIE FOR AUTHENTICATION (get from chrome dev tools) ex: "PANWEB=1; csrfToken=; lang=en; TSID=; __bid_n=; _ga=; __stripe_mid=; ndus=; browserid==; ndut_fmt=; _ga_06ZNKL8C2E=" (dont use this)
-ADMINS = [7193524341]
+ADMINS = [1895952308]
